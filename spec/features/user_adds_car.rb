0a8d541937cb36a14fb4c9756c7579467d218656
Modsky1 <maude.jake@gmail.com>
2014-04-14 10:08:38 -0400

    valid car entry feature spec

---
+++ spec/features/user_adds_car.rb
@@ -0,0 +1,27 @@
+require 'spec-helper'
+
+feature 'real estate associate adds building', %q{
+  As a car salesperson
+  I want to record a newly acquired car
+  So that I can list it in my lot
+  } do
+
+  scenario 'salesperson adds a car with valid attributes' do
+    car = FactoryGirl.build(:car)
+    count = Car.count
+
+    visit new_car_path
+    car_new_helper(car)
+
+    click_on 'Add Car'
+
+    expect(Car.count).to eq(count+1)
+    expect(page).to have_content("Car Successfully Added")
+    expect(current_path).to eq new_car_path
+  end
+
+  scenario 'salesperson adds a car with invalid attributes' do
+
+  end
+
+end
